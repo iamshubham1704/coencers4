c9d2f0b8fcca90b7670cd18337e4d6b65d90c64b
iamshubham1704 <sstech514@gmail.com>
2025-05-20 10:53:10 +0530

    responsive

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,9 +1,13 @@
-'use client'
-import Image from 'next/image'
-import styles from './Navbar.module.css'
-import Link from 'next/link'
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import styles from './Navbar.module.css';
+import Link from 'next/link';
+import { Menu, X } from 'lucide-react'; // Optional icon library
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
@@ -19,8 +23,14 @@ export default function Navbar() {
         </Link>
       </div>
 
+      {/* Hamburger icon */}
+      <div className={styles.menuIcon} onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <X size={24} /> : <Menu size={24} />}
+      </div>
 
-      <div className={styles.center}>
+      <div
+        className={`${styles.center} ${menuOpen ? styles.mobileMenuOpen : ''}`}
+      >
         <a href="/">Home</a>
         <a href="/reviews">Reviews</a>
         <a href="/about">About</a>
@@ -30,5 +40,5 @@ export default function Navbar() {
         <button className={styles.loginBtn}>Log In</button>
       </div>
     </nav>
-  )
+  );
 }
